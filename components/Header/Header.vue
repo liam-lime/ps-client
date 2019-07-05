56eaa1c56ea0eb21ba9e9a43e8351ef57d3f62e7
nazar.lynovetsky@skynix.co <nazar.lynovetsky@skynix.co>
2019-07-05 15:47:06 +0300

    Added GoogleFonts.

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="header">
-        <Section>
+        <div class="header__inner container">
             <Logo />
             <nav class="menu">
               <NuxtLink :to="$i18n.path('')" class="menu__link" exact>
@@ -10,7 +10,7 @@
                 {{ $t('links.about') }}
               </NuxtLink>
             </nav>
-        </Section>
+        </div>
     </header>
 </template>
 
@@ -25,3 +25,17 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.header {
+    padding: 20px 0;
+    background: #fff;
+    border-bottom: 1px solid #f1f1f1;
+    box-shadow: 0 4px 12px 0 rgba(0, 0, 0, .05);
+
+    &__inner {
+        display: flex;
+        justify-content: space-between;
+    }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
             { hid: 'description', name: 'description', content: 'Nuxt.js project' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=EB+Garamond:400,400i,500,500i,600,600i,700,700i,800,800i|Istok+Web:400,400i,700&display=swap&subset=cyrillic' }
         ]
     },
     modules: ['@nuxtjs/style-resources'],
