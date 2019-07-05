926d6e68dafb012ec3ef252dc88f6396541ae89a
nazar.lynovetsky@skynix.co <nazar.lynovetsky@skynix.co>
2019-07-05 13:42:11 +0300

    Added Button component.

--- components/Banners/CallForEmail.vue
+++ components/Banners/CallForEmail.vue
@@ -1,6 +1,21 @@
 <template>
-    <div class="call-for-email">
-        <h2 class="call-for-email__title">Interested in doing a project together?</h2>
-        <button class="call-for-email__button" type="button">Email me</button>
+    <div class="banner">
+        <h2>Interested in doing a project together?</h2>
+        <Button :onClick="onClickEmailButton">Email me</Button>
     </div>
 </template>
+
+<script>
+import Button from "~/components/Button/Button"
+
+export default {
+    components: {
+        Button
+    },
+    methods: {
+        onClickEmailButton() {
+            console.log('Test Button Clicked!')
+        }
+    }
+}
+</script>
---
+++ components/Button/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button
+        type="button"
+        class="button"
+        @click="onClick"
+    >
+        <slot>Button</slot>
+    </button>
+</template>
+
+<script>
+// source: https://codeburst.io/creating-reusable-components-with-vue-js-button-component-503167facfde
+export default {
+    props: {
+        onClick: {
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
