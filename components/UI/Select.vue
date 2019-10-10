721a2f9c0c5eeef83ef6732921fcc3abe899e5e0
nazar.lynovetsky@skynix.co <nazar.lynovetsky@skynix.co>
2019-10-10 13:21:13 +0300

    Save

--- components/UI/Select.vue
+++ components/UI/Select.vue
@@ -3,8 +3,10 @@
         <option disabled value="">Choose</option>
         <option
             v-for="(option, index) in options"
-            :key="index" :value="option.code"
-            :selected="selectedOption(option)">
+            :key="index"
+            :value="option.code"
+            
+        >
             {{ option.name }}
         </option>
     </select>
@@ -20,13 +22,17 @@ export default {
     },
     methods: {
         selectedOption(option) {
+            console.log('selectedOption: ', option, this, this.value);
             if (this.value) {
                 return option.code === this.value.code;
             }
             return false;
         },
-        change(e) {
-            const selectedCode = e.target.value;
+        change(event) {
+            console.log('event: ', event);
+            console.log('event target: ', event.target);
+            console.log('event target value: ', event.target.value);
+            const selectedCode = event.target.value;
             const option = this.options.find((option) => {
                 return selectedCode === option.code;
             });
