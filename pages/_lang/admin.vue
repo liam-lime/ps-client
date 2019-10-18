efb429cd1b9de700fe622485e8952a1ba9faf2fb
nazar.lynovetsky@skynix.co <nazar.lynovetsky@skynix.co>
2019-10-18 16:31:10 +0300

    Added admin page. Added new store/blog data objects.

---
+++ pages/_lang/admin.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="page">
+        <Section :padding="$mq | mq({ tabletBig: $padding.big })" v-border.bottom>
+            <Container>
+                <PageIntro :title="'Admin Panel'" />
+            </Container>
+        </Section>
+        <Section v-border.bottom>
+            <Container>
+                <SectionHeader :title="'Add new Post'" />
+                <h3>Title:</h3>
+                <input type="text" name="" value="">
+                <h3>Content:</h3>
+                <textarea name="name" rows="8" cols="80"></textarea>
+                <h3>Select category:</h3>
+                <select class="" name="">
+                    <option value="1">Vue.js</option>
+                    <option value="1">Javascript</option>
+                    <option value="1">Node.js</option>
+                    <option value="1">Web Design</option>
+                    <option value="1">React.js</option>
+                </select>
+            </Container>
+        </Section>
+    </div>
+</template>
+
+<script>
+import PageIntro from "~/components/Page/PageIntro"
+
+export default {
+    head() {
+        return { title: this.$t('about.title') }
+    },
+    components: {
+        PageIntro,
+    },
+    created() {
+        this.$store.commit('page/SET_TITLE', this.$t('about.page-title'))
+    }
+}
+</script>
